9b4b6794a76210b619f5c7d86186cf65f85f4205
Sema García <jose-manuel.garcia@gft.com>
2018-01-04 19:42:47 +0100

    feat(models): Adds some models

---
+++ src/models/playlist.ts
@@ -0,0 +1,11 @@
+// Playlist manipulation
+/*
+- Get a user's playlists
+- Create playlists
+- Change playlist details
+- Add tracks to a playlist
+- Remove tracks from a playlist
+- Replace tracks in a playlist
+- Reorder tracks in a playlist
+- Upload custom playlist cover image
+*/
