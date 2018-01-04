37f99bce72214aec56b9ee4daf88b601a8d78031
Sema García <jose-manuel.garcia@gft.com>
2018-01-04 19:44:08 +0100

    Adds spotify provider and reorder pages and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+// AppComponent
 import { MyApp } from './app.component';
+
+// Pages
 import { HomePage } from '../pages/home/home';
 import { PlayerPage } from '../pages/player/player';
 import { PlayMusicPage } from '../pages/play-music/play-music';
 import { PlayVideoPage } from '../pages/play-video/play-video';
 
+// Native
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { Hotspot } from '@ionic-native/hotspot';
+
+// Providers
+import { SpotifyWebProvider } from '../providers/spotify-web-api/spotify-web';
 
 @NgModule({
   declarations: [
@@ -35,8 +41,8 @@ import { Hotspot } from '@ionic-native/hotspot';
   providers: [
     StatusBar,
     SplashScreen,
-    Hotspot,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    SpotifyWebProvider,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
