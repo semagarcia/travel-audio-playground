08e34e557bc5c9dcc1400cf4fba163819e908fb4
Sema García <jose-manuel.garcia@gft.com>
2018-01-10 00:05:42 +0100

    feat((navigation): adds links for navigating

--- src/pages/manage-queue/manage-queue.ts
+++ src/pages/manage-queue/manage-queue.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 
+import { SearchByPlaylistPage } from './../search-by-playlist/search-by-playlist';
+import { SearchBySongPage } from './../search-by-song/search-by-song';
+
 @IonicPage({
   name: 'manage-queue'
 })
@@ -19,6 +22,14 @@ export class ManageQueuePage {
 
   ionViewDidEnter() {
 
+  }
+
+  search(searchType: string) {
+    if(searchType === 'playlist') {
+      this.navCtrl.push(SearchByPlaylistPage, { animate: true });
+    } else if(searchType === 'song') {
+      this.navCtrl.push(SearchBySongPage, { animate: true });
+    }
   }
 
 }
