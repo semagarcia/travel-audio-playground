1b3f3429881100603f98ffb8cccb1a77300ca2c2
Sema García <jose-manuel.garcia@gft.com>
2018-01-09 19:34:46 +0100

    firebase config

---
+++ src/config/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAyyr9WHK_IWtthrlM8TZ7ybT6lsqZMv50",
+  authDomain: "travel-audio-playground.firebaseapp.com",
+  databaseURL: "https://travel-audio-playground.firebaseio.com",
+  projectId: "travel-audio-playground",
+  storageBucket: "travel-audio-playground.appspot.com",
+  messagingSenderId: "612361177786"
+};
