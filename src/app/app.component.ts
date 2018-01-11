30b1b04fd7c6c6bcd6620a5b15792ca2d0f50052
Sema García <jose-manuel.garcia@gft.com>
2018-01-11 02:42:49 +0100

    Removes uneeded comments and change main navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,11 @@ import { PlayerPage } from '../pages/player/player';
 import { PlayMusicPage } from '../pages/play-music/play-music';
 import { PlayVideoPage } from '../pages/play-video/play-video';
 
-// Insert a barrel for providers
-import { SpotifyWebProvider } from './../providers/spotify-web-api/spotify-web';
-
 @Component({
   templateUrl: 'app.component.html'
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
-
   rootPage: any = LoginPage;
   pages: Array<{title: string, component: any}>;
 
@@ -26,13 +22,9 @@ export class MyApp {
     public platform: Platform,
     public statusBar: StatusBar,
     public splashScreen: SplashScreen,
-    // private spotifySrv: SpotifyWebProvider
   ) {
     this.initializeApp();
-
-    // used for an example of ngFor and navigation
     this.pages = [
-      //{ title: 'Login', component: LoginPage },
       { title: 'Home', component: HomePage },
       { title: 'Manage queue', component: ManageQueuePage },
       { title: 'Player', component: PlayerPage },
@@ -43,18 +35,12 @@ export class MyApp {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
   }
 
   openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    //this.nav.setRoot(page.component);
-
-    this.nav.push(page.component);
+    this.nav.setRoot(page.component);
   }
 }
